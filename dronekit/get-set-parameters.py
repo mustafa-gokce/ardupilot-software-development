197da2fc2d182eeb8bffb847809137cec5cd0d13
m <mustafagokce009@gmail.com>
2021-08-07 23:40:18 +0300

    worked on get set parameters and vehicle calibration

--- dronekit/get-set-parameters.py
+++ dronekit/get-set-parameters.py
@@ -1,12 +1,29 @@
 """
 Source: https://dronekit-python.readthedocs.io/en/latest/automodule.html
+        https://ardupilot.org/copter/docs/parameters.html
 """
 
+import time
 import dronekit
 
 print("Trying to connect to the vehicle...")
 vehicle = dronekit.connect(ip="127.0.0.1:14550", wait_ready=True)
 print("Connected to the vehicle.")
+
+# get a vehicle parameter
+print("ID of this vehicle: ", vehicle.parameters["SYSID_THISMAV"])
+
+time.sleep(5)
+
+# set a vehicle parameter
+vehicle.parameters["SYSID_THISMAV"] = 2
+
+time.sleep(5)
+
+# get a vehicle parameter
+print("ID of this vehicle: ", vehicle.parameters["SYSID_THISMAV"])
+
+time.sleep(5)
 
 print("Closing the vehicle...")
 vehicle.close()
--- dronekit/vehicle-calibrations.py
+++ dronekit/vehicle-calibrations.py
@@ -1,12 +1,39 @@
 """
 Source: https://dronekit-python.readthedocs.io/en/latest/automodule.html
+
+Request accelerometer calibration
+vehicle.send_calibrate_accelerometer()
+
+Request barometer calibration
+vehicle.send_calibrate_barometer()
+
+Request gyroscope calibration
+vehicle.send_calibrate_gyro()
+
+Request magnetometer calibration
+vehicle.send_calibrate_magnetometer()
+
+Request vehicle level calibration
+vehicle.send_calibrate_vehicle_level()
 """
 
+import time
 import dronekit
 
 print("Trying to connect to the vehicle...")
 vehicle = dronekit.connect(ip="127.0.0.1:14550", wait_ready=True)
 print("Connected to the vehicle.")
+
+time.sleep(5)
+print("Altitude: ", vehicle.location.global_relative_frame.alt)
+time.sleep(5)
+
+# request barometer calibration
+vehicle.send_calibrate_barometer()
+print("Requested barometer calibration")
+
+time.sleep(5)
+print("Altitude: ", vehicle.location.global_relative_frame.alt)
 
 print("Closing the vehicle...")
 vehicle.close()
