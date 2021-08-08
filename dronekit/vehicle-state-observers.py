a148c65e261ff8c6b8654cc334df7cbebcae4ff6
m <mustafagokce009@gmail.com>
2021-08-08 18:40:49 +0300

    worked on vehicle state observers lecture

--- dronekit/vehicle-state-observers.py
+++ dronekit/vehicle-state-observers.py
@@ -20,7 +20,7 @@ https://ardupilot.org/copter/docs/parameters.html
 You can add message listeners to the script and the listener function will be called
 whenever the specific message is received.
 vehicle.add_message_listener("MESSAGE_NAME", observer_function_name)
-To remove the attribute listener:
+To remove the message listener:
 vehicle.remove_message_listener("MESSAGE_NAME", observer_function_name)
 Message names can be found in MAVLink documentation:
 https://mavlink.io/en/messages/common.html
@@ -29,16 +29,35 @@ https://mavlink.io/en/messages/common.html
 import time
 import dronekit
 
+
+def mode_callback(self, attribute_name, message):
+    print(self.mode.name)
+
+
+def parameter_callback(self, parameter_name, message):
+    print(parameter_name, message)
+
+
+def message_callback(self, message_name, message):
+    print("Heartbeat:", self.last_heartbeat)
+
+
 print("Trying to connect to the vehicle...")
 vehicle = dronekit.connect(ip="127.0.0.1:14550", wait_ready=True)
 print("Connected to the vehicle.")
 
+# vehicle.add_attribute_listener("mode", mode_callback)
+# vehicle.parameters.add_attribute_listener("SYSID_THISMAV", parameter_callback)
+vehicle.add_message_listener("HEARTBEAT", message_callback)
 
 # dummy loop waiting for keyboard interrupt
 try:
     while True:
         time.sleep(0.1)
 except KeyboardInterrupt:
+    # vehicle.remove_attribute_listener("mode", mode_callback)
+    # vehicle.parameters.remove_attribute_listener("SYSID_THISMAV", parameter_callback)
+    vehicle.remove_message_listener("HEARTBEAT", message_callback)
     print("Closing the vehicle...")
     vehicle.close()
     print("Closed the vehicle.")
